62c3156e5efbc4c6f02ed7f512259659ed7c60e3
seven <lvtianpeng93@gmail.com>
2023-11-11 18:09:01 +0800

    refactor(test): [iframe] main postMessage Restrictions(whitelist)

--- src/entry/iframe/main/test/whitelist.test.ts
+++ src/entry/iframe/main/test/whitelist.test.ts
@@ -6,45 +6,46 @@ import { postMessageMock } from "../../../../mock/postMessage.mock";
 
 describe("Main postMessage Restrictions 1", () => {
   it("Origin: White List", () => {
+    const origin = "https://www.iframe.com";
     let iframe = window.document.createElement("iframe");
-    iframe.src = "https://www.iframe.com";
+    iframe.src = origin;
     window.document.body.appendChild(iframe);
 
-    let data;
-    let data1;
+    let inWhitelistData;
+    let notInWhitelistData;
     const messageDispatcher = {
-      ping(r) {
-        data = r;
+      inWhitelist(r) {
+        inWhitelistData = r;
       },
-      whitelist(r) {
-        data1 = r;
+      notInWhiteList(r) {
+        notInWhitelistData = r;
       },
     };
     registerMain(messageDispatcher, {
       iframeDom: iframe,
-      originWhiteList: ["https://www.iframe.com"],
+      originWhiteList: [origin],
     });
 
     return new Promise<void>((resolve) => {
-      const msg: MESSAGE_DATA = {
+      const inWhitelistMsg: MESSAGE_DATA = {
         messageType: MESSAGE_TYPE.REQUEST,
-        callName: "ping",
+        callName: "inWhitelist",
         messageId: "2",
         data: PING,
       };
-      const msg1: MESSAGE_DATA = {
+      const notInWhiteListMsg: MESSAGE_DATA = {
         messageType: MESSAGE_TYPE.REQUEST,
-        callName: "whitelist",
+        callName: "notInWhiteList",
         messageId: "3",
         data: PING,
       };
       // iframe.contentWindow!.parent.postMessage(msg, "*"); // JSDOM postMessage do not set event.origin
-      postMessageMock(msg, "https://www.iframe.com");
-      postMessageMock(msg1, "https://www.iframe_1.com");
+      postMessageMock(inWhitelistMsg, origin);
+      postMessageMock(notInWhiteListMsg, "https://www.iframe_1.com");
 
       setTimeout(() => {
-        expect(data).toEqual(PING);
-        expect(data1).toEqual(void 0);
+        expect(inWhitelistData).toEqual(PING);
+        expect(notInWhitelistData).toEqual(void 0);
         resolve();
       }, 1000);
     });
